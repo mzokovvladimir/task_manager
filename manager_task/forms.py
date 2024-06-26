914d1a5ac2996cee51c3c1c41f1e895ab2d5672b
Anton Nechay <a.a.nechay@gmail.com>
2024-06-26 22:55:21 +0300

    commit front 26.06

--- manager_task/forms.py
+++ manager_task/forms.py
@@ -3,12 +3,30 @@ from .models import Task
 
 
 class TaskForm(forms.ModelForm):
+    due_date = forms.DateField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control datepicker',
+                'placeholder': 'YYYY-MM-DD',
+                'style': 'width: 30%; height: auto; margin: 0 auto;'
+            }
+        )
+    )
     class Meta:
         model = Task
         fields = ['title', 'description', 'due_date', 'priority']
 
 
 class EditTaskForm(forms.ModelForm):
+    due_date = forms.DateField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control datepicker',
+                'placeholder': 'YYYY-MM-DD',
+                'style': 'width: 30%; height: auto; margin: 0 auto;'
+            }
+        )
+    )
     class Meta:
         model = Task
         fields = ['title', 'description', 'due_date', 'priority', 'completed']
